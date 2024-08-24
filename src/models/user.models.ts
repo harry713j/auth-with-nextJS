8f904f0900bf77b7f61f59124613f1a5f6357157
Harry <hariharasethi89@gmail.com>
2024-08-24 15:38:45 +0530

    Initilize set up, make user model and make mailing function

---
+++ src/models/user.models.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+// NextJS does edge computing so it doesn't know a schema is already present or it is new
+// That's why we put a checking
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
