f766b26b976a2cbf4abecd54e0e16e3e350fbe32
Harry <hariharasethi89@gmail.com>
2024-08-26 09:03:45 +0530

    Define all the routes

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,40 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/user.models";
+import { NextRequest, NextResponse } from "next/server";
+import { sendEmail } from "@/helpers/mailer"; // TODO: use this to send successfully verified message to user
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+    console.log(token);
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Invalid token" }, { status: 400 });
+    }
+
+    console.log(user);
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      {
+        message: "Email verified successfully",
+        success: true,
+      },
+      { status: 500 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
